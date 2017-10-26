f2f48ad6ac39b5cba5562a1358d8406b8dbad630
Quinn <qtoconnor@gmail.com>
2017-10-26 17:44:12 -0500

    added packages and classes for each package

---
+++ src/model/Guard.java
@@ -0,0 +1,89 @@
+package model;
+
+public class Guard {
+	String name;
+	int age;
+	int intime;
+	int breakout1;
+	int breakin1;
+	int breakout2;
+	int breakin2;
+	int outtime;
+	String status;
+	String position;
+	
+//
+// Initialize Guard
+//
+	public Guard (String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	
+//
+// Time in
+//
+	public void setInTime (int time) {
+		intime = time;
+	}
+	
+	
+//
+// Time out
+//
+	public void setOutTime (int time) {
+		outtime = time;
+	}	
+	
+	
+//
+// First break
+//
+	public void setbreakin1 (int time) {
+		breakin1 = time;
+	}
+	
+	public int getbreakin1 () {
+		return breakin1;
+	}
+	
+	public void setbreakout1 (int time) {
+		breakout1 = time;
+	}
+	
+	public int getbreakout1 () {
+		return breakout1;
+	}
+	
+	
+//
+// Second break
+//
+	public void setbreakin2 (int time) {
+		breakin1 = time;
+	}
+	
+	public void setbreakout2 (int time) {
+		breakout1 = time;
+	}
+	
+	
+//
+// Status
+//
+	public void setStatus (String stat) {
+		status = stat;
+	}
+	
+	
+//
+// Position
+//
+	public void setPosition (String pos) {
+		position = pos;
+	}
+	
+	
+	
+}
---
+++ src/model/Test.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Guard quinn = new Guard("Quinn", 20);
+		Guard brad = new Guard("Brad", 20);
+		
+		quinn.setStatus("doing nothing!");
+		brad.setStatus("doing Everything!");
+		
+		System.out.println(quinn.name + " is " + quinn.status);
+		System.out.println(brad.name + " is " + brad.status);
+		
+		Rotation one = new Rotation();
+
+	}
+
+}
