65aaa0f09b2a0dc4f4217da18750be35f162f6b0
Quinn <qtoconnor@gmail.com>
2017-10-29 19:41:19 -0500

    deleted poop. changed setStatus in guard class

--- src/model/Test.java
+++ src/model/Test.java
@@ -1,20 +1,26 @@
 package model;
 
+import java.util.*;
+
 public class Test {
 
 	public static void main(String[] args) {
 		Guard quinn = new Guard("Quinn", 20);
 		Guard brad = new Guard("Brad", 20);
 		
-		quinn.setStatus("doing nothing!");
-		brad.setStatus("doing Everything!");
+		quinn.setStatus(1);
+		brad.setStatus(3);
 		
 		System.out.println(quinn.name + " is " + quinn.status);
 		System.out.println(brad.name + " is " + brad.status);
 		
 		//Rotation one = new Rotation();
 		//this now needs a name, age req, list of positions, and list of guards to initialize
-
+		
+		List<Guard> guardList= new LinkedList<Guard>();
+		guardList.add(quinn);
+		guardList.add(brad);
+		
 	}
 
 }
