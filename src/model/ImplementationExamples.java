418d515bad970816cd2c9abccf208b3db44def54
BradHennesSchool <bhennes14@winona.edu>
2017-11-08 16:58:13 -0600

    fixed things

--- src/model/ImplementationExamples.java
+++ src/model/ImplementationExamples.java
@@ -1,12 +1,16 @@
 package model;
-
+import java.util.*;
 public class ImplementationExamples 
 {
 	public static void main(String[] args) 
 	{
-		String[][] input = new String[6][7];
+		ArrayList<Guard> GuardList = new ArrayList<Guard>();
 		
-		
+		//foreach record in the database
+		for(int i = 0; i < 5; ++i)
+		{
+			GuardList.add(new Guard("Brad" + i, "Hennes" + i, 19, i));
+		}
 		
 		
 		
