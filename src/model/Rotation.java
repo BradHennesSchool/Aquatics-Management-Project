d7c345b8391dda97be97ec5761af0d4b79670e51
BradHennesSchool <bhennes14@winona.edu>
2017-11-08 08:23:09 -0600

    set resizable to true

--- src/model/Rotation.java
+++ src/model/Rotation.java
@@ -2,26 +2,26 @@ package model;
 
 public class Rotation {
 
-		String name;
-		int ageReq;
-		
-		Position[] Positions;
-		
-		public Rotation(String name, int ageReq, Position[] PositionList, Guard[] InitGuardList){
-			this.name = name;
-			this.ageReq = ageReq;
-			this.Positions = new Position[PositionList.length];
-			SetPositions(PositionList, InitGuardList);
+	String name;
+	int ageReq;
+	
+	Position[] Positions;
+	
+	public Rotation(String name, int ageReq, Position[] PositionList, Guard[] InitGuardList){
+		this.name = name;
+		this.ageReq = ageReq;
+		this.Positions = new Position[PositionList.length];
+		SetPositions(PositionList, InitGuardList);
+	}
+	
+	private void SetPositions(Position[] PositionList, Guard[] InitGuardList) {
+		for(int i = 0; i < PositionList.length; ++i){
+			Positions[i] = PositionList[i];
+			Positions[i].SetGuard(InitGuardList[i]);
 		}
-		
-		private void SetPositions(Position[] PositionList, Guard[] InitGuardList) {
-			for(int i = 0; i < PositionList.length; ++i){
-				Positions[i] = PositionList[i];
-				Positions[i].SetGuard(InitGuardList[i]);
-			}
-		}
-		
-		public Position[] Positions() {
-			return Positions;
-		}
+	}
+	
+	public Position[] Positions() {
+		return Positions;
+	}
 }
