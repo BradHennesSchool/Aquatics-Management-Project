9a89dee453db1a030133237da9a31406c96cf3d5
jeanleow <hleow15@winona.edu>
2017-11-13 12:02:31 -0600

    Update DatatoDB.java

--- src/Aqua/DatatoDB.java
+++ src/Aqua/DatatoDB.java
@@ -18,24 +18,24 @@ public class DatatoDB {
 		return instance;
 	}
 	
-		//Establish connection to MySQL server
-		public Connection newConnection() {
-			try {
-				Class.forName("com.mysql.jdbc.Driver").newInstance();
-				
-				try {			
-					dbconn = DriverManager.getConnection(dbPath,"root","1995");
-					System.out.println("gain the connection");
-					return dbconn;
-				}
-				catch (Exception s){
-					System.out.println(s.getStackTrace().toString());}
+	//Establish connection to MySQL server
+	public Connection newConnection() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			
+			try {			
+				dbconn = DriverManager.getConnection(dbPath,"root","1995");		//1995 is my db password
+				System.out.println("gain the connection");
+				return dbconn;
 			}
-			catch (Exception err){
-				System.out.println(err.getStackTrace().toString());
-			}
-			return null;
+			catch (Exception s){
+				System.out.println(s.getStackTrace().toString());}
 		}
+		catch (Exception err){
+			System.out.println(err.getStackTrace().toString());
+		}
+		return null;
+	}
 	
 	public ResultSet selectStatement( String query ) {
 		try {
@@ -71,12 +71,12 @@ public class DatatoDB {
 			return false;
 		}
 	}
-		
-	public boolean entryGuardInfo( String fname, String lname, String age ) {
+	
+	public boolean entryGuardInfo( String Fname, String Lname, String Age ) {
 		try {
 	
-			instance.DBentry("INSERT INTO Aquatic_Mgmt.Guard_Info ( `fname`, `lname`, `age`) " +
-								"VALUES ('"+fname+"', '"+lname+"' ,'"+age+"');");	
+			instance.DBentry("INSERT INTO Algae.Guard_Info ( `Fname`, `Lname`, `Age`) " +
+								"VALUES ('"+Fname+"', '"+Lname+"' ,'"+Age+"');");	
 			return true;
 		}
 		catch ( Exception err ) {
@@ -85,11 +85,11 @@ public class DatatoDB {
 		}
 	}
 	
-	public boolean enterRotation( String TL_name, String G_name, String Rot_Time, String L1, String L2, String L3, String L4, String L5 ) {
+	public boolean enterRotation( String TL_name, String G_name, String Rot_Time, String Position1, String Position2, String Position3, String Position4, String Position5, String Age ) {
 		try {
 	
-			instance.DBentry("INSERT INTO Aquatic_Mgmt.Rotation ( `Team_Lead`, `Gname`, `Ro_Time`, `Position1`, `Position2`, `Position3`, `Position4`, `Position5`) " +
-								"VALUES ('"+TL_name+"', '"+G_name+"' ,'"+Rot_Time+"', '"+L1+"', '"+L2+"', '"+L3+"','"+L4+"', '"+L5+"');");	
+			instance.DBentry("INSERT INTO Algae.Rotation ( `Team_Lead`, `Ro_Time`, `Position1`, `Position2`, `Position3`, `Position4`, `Position5`, `age`) " +
+								"VALUES ('"+TL_name+"','"+Rot_Time+"', '"+Position1+"', '"+Position2+"', '"+Position3+"','"+Position4+"', '"+Position5+"', '"+Age+"');");	
 			return true;
 		}
 		catch ( Exception err ) {
@@ -98,11 +98,11 @@ public class DatatoDB {
 		}
 	}
 	
-	public boolean enterSchWork( String fname, String sch_day, String sch_in_time, String sch_out_time, String sch_date ) {
+	public boolean enterSchWork( String Fname, String Sch_day, String Sch_in_time, String Sch_out_time, String Sch_date ) {
 		try {
 	
-			instance.DBentry("INSERT INTO Aquatic_Mgmt.ScheduleWork ( `G_name`, `Sch_day`, `Sch_in_time`, `Sch_out_time`, `Sch_date`) " +
-								"VALUES ('"+fname+"', '"+sch_day+"' ,'"+sch_in_time+"', '"+sch_out_time+"', '"+sch_date+"' );");	
+			instance.DBentry("INSERT INTO Algae.ScheduleWork ( `G_name`, `Sch_day`, `Sch_in_time`, `Sch_out_time`, `Sch_date`) " +
+								"VALUES ('"+Fname+"', '"+Sch_day+"' ,'"+Sch_in_time+"', '"+Sch_out_time+"', '"+Sch_date+"' );");	
 			return true;
 		}
 		catch ( Exception err ) {
@@ -111,11 +111,11 @@ public class DatatoDB {
 		}
 	}
 	
-	public boolean enterActWork( String fname, String Act_day, String Act_in_time, String Act_out_time, String Act_date ) {
+	public boolean enterActWork( String Fname, String Act_day, String Act_in_time, String Act_out_time, String Act_date, String Break_in1, String Break_out1, String Break_in2, String Break_out2 ) {
 		try {
 	
-			instance.DBentry("INSERT INTO Aquatic_Mgmt.ActualWork ( `G_name`, `Act_day`, `Act_in_time`, `Act_out_time`, `Act_date`) " +
-								"VALUES ('"+fname+"', '"+Act_day+"' ,'"+Act_in_time+"', '"+Act_out_time+"', '"+Act_date+"' );");	
+			instance.DBentry("INSERT INTO Algae.ActualWork ( `G_name`, `Act_day`, `Act_in_time`, `Act_out_time`, `Act_date`, `Break_in1`, `Break_out1`, `Break_in1`, `Break_out2` ) " +
+								"VALUES ('"+Fname+"', '"+Act_day+"' ,'"+Act_in_time+"', '"+Act_out_time+"', '"+Act_date+"', '"+Break_in1+"', '"+Break_out1+"', '"+Break_in2+"', '"+Break_out2+"' );");	
 			return true;
 		}
 		catch ( Exception err ) {
@@ -123,6 +123,5 @@ public class DatatoDB {
 			return false;
 		}
 	}
-	
 	
 }
